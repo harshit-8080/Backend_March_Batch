db6dc3775a04ad6e683633627dc71f5e8391007d
Harshit Raj <41303856+harshit-8080@users.noreply.github.com>
2023-04-15 00:07:05 +0530

    health status code added

--- ipl_backend/server.js
+++ ipl_backend/server.js
@@ -10,12 +10,16 @@ app.use(bodyParser.json());
 app.use(TeamRouter);
 app.use(PlayerRouter);
 app.use(AdminRouter);
+app.get("/", (req, res) => {
+  res.json({
+    message: "Ok",
+    response: "health check added",
+  });
+});
 
 app.listen(3000, async () => {
   console.log("server listening on port 3000");
   await db_connection();
 });
-
-
 
 // how are we testing the server
